7925a2ebc666328204583507bba7deeec28ac9cf
DaveKimesh <murangiridavis1@gmail.com>
2023-02-22 09:21:48 -0500

    a function that checks for alphabetic character. Prototype: int _isalpha(int c);Returns 1 if c is a letter, lowercase or uppercase. Returns 0 otherwise

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_isalpha - to check whether a character is uppercase
+*@c: The character to be checked
+*Return: void
+*/
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+		else
+		return (0);
+}
